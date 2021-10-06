554c2e646aac9b1ef00fc543cff5ae18e71e8507
Alin Manole <alin.manole@yahoo.com>
2021-10-06 16:23:19 +0200

    Table-Utilis Push

---
+++ multiply.js
@@ -0,0 +1,22 @@
+process.argv
+var arguments = process.argv.slice(2)
+
+var num = parseInt(arguments[0])
+
+function multiply(num) {
+    if (!num) {
+        console.log("Error !")
+    } else if ( num === num ) {
+        for( var i = 0 ; i <= 10; i++){
+            var result = num * i
+            console.log(`${num} x ${i} = ${result}`)
+        }
+    } else  {
+        console.log("Error !")
+    }
+}
+
+multiply(num)
+
+
+
